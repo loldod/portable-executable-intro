b32c2772548558bb5b70c0972cfe1bdbeb0ef5d2
Liron Danan <lirondanan12@gmail.com>
2026-09-14 22:32:22 +0300

    loading library, printing its address and magic

--- portable-executable-intro/portable-executable-intro.cpp
+++ portable-executable-intro/portable-executable-intro.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <windows.h>
 
 int main()
 {
+    HMODULE libraryPtr = nullptr;
+
+    libraryPtr = LoadLibrary(L"Kernel32.dll");
+    if (libraryPtr == nullptr) {
+        std::cout << "cannot find address to Kernel32.dll!";
+        return 1;
+    }
+    std::cout << "Kernel32.dll pointer address: " << libraryPtr << std::endl;
+
+    PIMAGE_DOS_HEADER imageDosHeaderPtr = (PIMAGE_DOS_HEADER)libraryPtr;
+    std::cout << "DLL magic: " << imageDosHeaderPtr->e_magic << std::endl;
     
+    FreeLibrary(libraryPtr);
     return 0;
 }
