d2b88ee3246359ec7f92fe83ef316d67a7db2677
Liron Danan <lirondanan12@gmail.com>
2026-09-14 22:58:55 +0300

    read image nt headers and print cpu architecture

--- portable-executable-intro/portable-executable-intro.cpp
+++ portable-executable-intro/portable-executable-intro.cpp
@@ -12,9 +12,20 @@ int main()
     }
     std::cout << "Kernel32.dll pointer address: " << libraryPtr << std::endl;
 
-    PIMAGE_DOS_HEADER imageDosHeaderPtr = (PIMAGE_DOS_HEADER)libraryPtr;
-    std::cout << "DLL magic: " << imageDosHeaderPtr->e_magic << std::endl;
-    
+    PIMAGE_DOS_HEADER imageDosHeader = (PIMAGE_DOS_HEADER)libraryPtr;
+    std::cout << "DLL magic: " << imageDosHeader->e_magic << std::endl;
+
+    PIMAGE_NT_HEADERS imageNtHeaders = (PIMAGE_NT_HEADERS)((byte*)libraryPtr + imageDosHeader->e_lfanew);
+    if (IMAGE_FILE_MACHINE_AMD64 == imageNtHeaders->FileHeader.Machine) {
+        std::cout << "The DLL is 64 bit";
+    }
+    else if (IMAGE_FILE_MACHINE_I386 == imageNtHeaders->FileHeader.Machine) {
+        std::cout << "The DLL is 32 bit";
+    }
+    else {
+        std::cout << "The DLL is not 64 bit or 32 bit!";
+    }
+
     FreeLibrary(libraryPtr);
     return 0;
 }
